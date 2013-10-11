0bdb8ad880be8d369cd15f8db854b41f44dd872c
meteorakey <meteorakey@gmail.com>
2013-10-11 16:58:17 +0900

    カード作成を分離

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ class Main {
 	public static void main(String[] args) {
 
 		AccountManager manager = new AccountManager();
-		manager.makeAccount("Taro", true);
-
+		manager.makeAccount("Taro", false);
 		Account account = manager.getAccount("Taro");
+		manager.makeCard(account);
 		if(account.getCard() != null) {
 			System.out.println("Taro has a card");
 			account.setTicket(new Ticket("Taro"));
